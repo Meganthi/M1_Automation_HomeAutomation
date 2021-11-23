1a8443654c74c43c2cfaa9dd8433d1af15fe4523
Meganthi <94211854+Meganthi@users.noreply.github.com>
2021-11-23 19:27:13 +0530

    Update buzzer.c

--- 3_Implementation/src/buzzer.c
+++ 3_Implementation/src/buzzer.c
@@ -1,1 +1,21 @@
-
+/**
+ * @file main.c
+ * @author meganthi
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+void buzzer(int k)
+{
+ int j;
+ for(j=0;j<k;j++){
+ digitalWrite(buzzerPin,HIGH);
+ delay(500);
+ digitalWrite(buzzerPin,LOW);
+ delay(500);}
+}
