34066341e75f7b9f8ccfe0da8205efba2cc86d58
Meganthi <94211854+Meganthi@users.noreply.github.com>
2021-11-23 13:10:53 +0530

    Create homeautomation.h

---
+++ 3_Implementation/inc/homeautomation.h
@@ -0,0 +1,20 @@
+/**
+ * @file homeautomation.h
+ * @author meganthi
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+
+
+#define bulb_1Pin A0
+#define bulb_2Pin A1
+#define bulb_3Pin A2
+#define fanPin A3
+#define buzzerPin A4
+
